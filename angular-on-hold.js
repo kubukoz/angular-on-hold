57c230987cb5c11e635d6ef97c26c891f9cea168
Jakub Kozłowski <kubukoz@gmail.com>
2015-07-27 20:01:07 +0200

    Create angular-on-hold.js

---
+++ angular-on-hold.js
@@ -0,0 +1,26 @@
+var module = angular.module("kzHold", []);
+module.directive("kzHold", function($interval){
+  return {
+      scope: {kzHold: "=", frequency: "="},
+      link: function(scope, elem, attr){
+          var action = scope.kzHold;
+          var intervalSet;
+          var intervalPromise;
+          var bindAction = function(){
+              if(!intervalSet) {
+                  intervalPromise = $interval(action, 1000/(scope.frequency || 180));
+                  intervalSet=true;
+              }
+          };
+          var endAction = function(){
+              if(intervalSet){
+                $interval.cancel(intervalPromise);
+                intervalSet=false;
+              }
+          };
+          elem.on("mousedown", bindAction);
+          elem.on("mouseup", endAction);
+          elem.on("mouseleave", endAction);
+      }
+  };
+});
